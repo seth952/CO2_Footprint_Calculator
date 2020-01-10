ff33514bf0041bc0d056ad8dc989cc44e0a20881
User <user@Users-MacBook-Pro.local>
2020-01-10 15:36:46 +0000

    server details added

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const createRouter = require('./helpers/create_router.js');
 
+app.use(cors());
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('emissions');
+  const emissionsCollection = db.collection('emissions');
+  const emissionsRouter = createRouter(emissionsCollection);
+  app.use('/api/emissions', emissionsRouter);
+})
+.catch(console.err)
+
+app.listen(3000, function(){
+  console.log("App running on port 3000");
+})
