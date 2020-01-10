2dc2fc1e63dc918f30689c20c4ad81b569c0008d
Paul McManus <paulmcmanus2021@hotmail.com>
2020-01-10 16:56:36 +0000

    Adds user seed to Api

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,18 +2,21 @@ use emissions;
 
 db.dropDatabase()
 
-db.emissions.insertOne(
+db.emissions.insertMany([
 
-  {
-    car: "0.6",
-    train: "0.3",
-    plane: "1.0",
-    meat: "0.5",
-    veg: "0.2",
-    electricity: "0.99",
-    gas: "0.33",
-    hybrid: "0.66"
+{ car: "0.6",
+  train: "0.3",
+  plane: "1.0",
+  meat: "0.5",
+  veg: "0.2",
+  electricity: "0.99",
+  gas: "0.33",
+  hybrid: "0.66"
+},
+{ name: "Paul McManus",
+  travelnumber: 12,
+  dietnumber: 10,
+  energynumber: 8
+}
 
-  }
-
-);
+]);
