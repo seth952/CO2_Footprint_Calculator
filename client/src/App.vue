5d7af42be364e21dd5f0858330848580980040ec
User <user@Users-MacBook-Pro.local>
2020-01-11 14:34:47 +0000

    componenets added, app.vue updated' '

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div id="">
-
+  <div id="app">
+<h1>Grant,Paul and Stephen CO2 tracker</h1>
+<footprint-running-total/>
+<forms-live-here/>
   </div>
 </template>
 <script>
+import FootprintRunningTotal from '@/components/FootprintRunningTotal';
+import FormsLiveHere from '@/components/FormsLiveHere';
 export default {
-  name: "",
-  data: () => ({
-
-  })
+  name: "app",
+  components: {
+    'footprint-running-total': FootprintRunningTotal,
+    'forms-live-here': FormsLiveHere
+  }
 }
 </script>
 <style lang="css" scoped>
