e7142afc6705689b3bffa288a26412bae5338170
Grant Houston <grant.houston44@hotmail.com>
2020-01-11 20:36:32 +0000

    updating branch with all changes to running total

--- client/src/components/FootprintRunningTotal.vue
+++ client/src/components/FootprintRunningTotal.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="running-total-container">
-      <h3 id="travelrunningtotal">Travel Total:</h3>
+      <h3 id="travelrunningtotal" v-if="travelRunningTotal != null">Travel Total: {{travelRunningTotal}}</h3>
       <h3 id="energyrunningtotal">Energy Total:</h3>
       <h3 id="dietrunningtotal">Diet Total:</h3>
       <h2 id="combinedrunningtotal">Combined Running Total:</h2>
@@ -16,23 +16,45 @@ export default {
   name: 'footprint-running-total',
   data(){
     return {
-      travelrunningtotal: [],
-      energyrunningtotal: [],
-      dietrunningtotal: [],
-      combinedrunningtotal: [],
+      travelrunningtotal: null,
+      travelactualrunningtotal: null,
+      energyrunningtotal: null,
+      dietrunningtotal: null,
       emissions: [],
 
     }
   },
+  computed: {
+    travelRunningTotal(){
 
+      let travelArray = []
+      travelArray.push(this.travelrunningtotal.car * this.emissions[0].travel.car)
+      travelArray.push(this.travelrunningtotal.train * this.emissions[0].travel.train)
+      travelArray.push(this.travelrunningtotal.plane * this.emissions[0].travel.plane)
+
+      const sumTravel = function(someArray){
+        return someArray.reduce((total, travel) => {
+            return total + travel
+        }, 0)
+
+      }
+      return this.travelactualrunningtotal = sumTravel(travelArray)
+    }
+  },
   mounted() {
     EmissionFactorsService.getEmissionFactors()
     .then(emissions => this.emissions = emissions)
     // UserDataService.getUserData()
     // .then(users => this.users = users)
 
-      eventBus.$on('running-total', (changeFootprint)=> {
-      this.travelrunningtotal.push(changeFootprint)
+      eventBus.$on('running-total-travel', (changeFootprint)=> {
+      this.travelrunningtotal = changeFootprint
+    })
+      eventBus.$on('running-total-energy', (changeFootprint)=> {
+      this.energyrunningtotal = changeFootprint
+    })
+      eventBus.$on('running-total-diet', (changeFootprint)=> {
+      this.dietrunningtotal = changeFootprint
     })
     }
   }
--- client/src/components/FormsLiveHere.vue
+++ client/src/components/FormsLiveHere.vue
@@ -5,34 +5,34 @@
         <h3>Travel</h3>
         <p>Enter miles traveled by:</p>
         <label for="car">Car:</label>
-        <input type="text" name="car" @change="handleChange" v-model="car">
+        <input type="text" name="car" @input="handleRunningTravel" v-model="car">
         <br>
         <label for="train">Train:</label>
-        <input type="text" name="train" v-model="train">
+        <input type="text" name="train" @input="handleRunningTravel" v-model="train">
         <br>
         <label for="plane">Plane:</label>
-        <input type="text" name="plane" v-model="plane">
+        <input type="text" name="plane" @input="handleRunningTravel" v-model="plane">
       </div>
       <div id="energy-form">
         <h3>Energy</h3>
         <p>Enter kilowatts used:</p>
         <label for="electricity">Electricity:</label>
-        <input type="text" name="electricity" v-model="electricity">
+        <input type="text" name="electricity" @input="handleRunningEnergy" v-model="electricity">
         <br>
         <label for="gas">Gas:</label>
-        <input type="text" name="gas" v-model="gas">
+        <input type="text" name="gas" @input="handleRunningEnergy" v-model="gas">
         <br>
         <label for="hybrid">Hybrid:</label>
-        <input type="text" name="hybrid" v-model="hybrid">
+        <input type="text" name="hybrid" @input="handleRunningEnergy" v-model="hybrid">
       </div>
       <div id="diet-form">
         <h3>Diet</h3>
         <p>Enter kg eaten in:</p>
         <label for="meat">Meat:</label>
-        <input type="text" name="meat" v-model="meat">
+        <input type="text" name="meat" @input="handleRunningDiet" v-model="meat">
         <br>
         <label for="veg">Veg:</label>
-        <input type="text" name="veg" v-model="veg">
+        <input type="text" name="veg" @input="handleRunningDiet" v-model="veg">
       </div>
     </form>
     <br>
@@ -78,15 +78,30 @@ export default {
     // });
   },
 
-  handleChange(){
-    const changeFootprint = {
-      car: this.car
-      }
+  handleRunningTravel(){
+    const runningFootprint = {
+      car: this.car,
+      train: this.train,
+      plane: this.plane
+        }
+      eventBus.$emit('running-total-travel', runningFootprint)
+  },
+  handleRunningEnergy(){
+    const runningFootprint = {
+      electricity: this.electricity,
+      gas: this.gas,
+      hybrid: this.hybrid
+        }
+      eventBus.$emit('running-total-energy', runningFootprint)
+  },
+  handleRunningDiet(){
+    const runningFootprint = {
+      meat: this.meat,
+      veg: this.veg
+        }
+      eventBus.$emit('running-total-diet', runningFootprint)
+  },
 
-      eventBus.$emit('running-total', changeFootprint);
-
-
-  }
 
 
 }
