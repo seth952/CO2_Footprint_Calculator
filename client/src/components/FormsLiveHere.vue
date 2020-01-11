61878599461db01c08bbcab09c4015fa771b37ba
Grant Houston <grant.houston44@hotmail.com>
2020-01-11 16:25:19 +0000

    services

---
+++ client/src/components/FormsLiveHere.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+
+</template>
+
+<script>
+import EmissionFactorsService from '@/services/EmissionFactorsService.js'
+import UserDataService from '@/services/UserDataService'
+
+export default {
+  name: 'test',
+  data(){
+    return {
+      emissions: [],
+      users: []
+    }
+  },
+  mounted() {
+    EmissionFactorsService.getEmissionFactors()
+    .then(emissions => this.emissions = emissions)
+    UserDataService.getUserData()
+    .then(users => this.users = users)
+
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
