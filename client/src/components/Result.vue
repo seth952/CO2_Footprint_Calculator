126fe4527783527e4bc1d01d8a3930040efcb551
Grant Houston <grant.houston44@hotmail.com>
2020-01-14 14:12:08 +0000

    drilldown now works

--- client/src/components/Result.vue
+++ client/src/components/Result.vue
@@ -50,7 +50,7 @@ export default {
 
     tooltip: {
         headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of bananas<br/>'
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of Total Footprint<br/>'
     },
 
     series: [
@@ -64,222 +64,7 @@ export default {
     ],
     drilldown: {
         series: [
-            {
-                name: "Chrome",
-                id: "Chrome",
-                data: [
-                    [
-                        "v65.0",
-                        0.1
-                    ],
-                    [
-                        "v64.0",
-                        1.3
-                    ],
-                    [
-                        "v63.0",
-                        53.02
-                    ],
-                    [
-                        "v62.0",
-                        1.4
-                    ],
-                    [
-                        "v61.0",
-                        0.88
-                    ],
-                    [
-                        "v60.0",
-                        0.56
-                    ],
-                    [
-                        "v59.0",
-                        0.45
-                    ],
-                    [
-                        "v58.0",
-                        0.49
-                    ],
-                    [
-                        "v57.0",
-                        0.32
-                    ],
-                    [
-                        "v56.0",
-                        0.29
-                    ],
-                    [
-                        "v55.0",
-                        0.79
-                    ],
-                    [
-                        "v54.0",
-                        0.18
-                    ],
-                    [
-                        "v51.0",
-                        0.13
-                    ],
-                    [
-                        "v49.0",
-                        2.16
-                    ],
-                    [
-                        "v48.0",
-                        0.13
-                    ],
-                    [
-                        "v47.0",
-                        0.11
-                    ],
-                    [
-                        "v43.0",
-                        0.17
-                    ],
-                    [
-                        "v29.0",
-                        0.26
-                    ]
-                ]
-            },
-            {
-                name: "Firefox",
-                id: "Firefox",
-                data: [
-                    [
-                        "v58.0",
-                        1.02
-                    ],
-                    [
-                        "v57.0",
-                        7.36
-                    ],
-                    [
-                        "v56.0",
-                        0.35
-                    ],
-                    [
-                        "v55.0",
-                        0.11
-                    ],
-                    [
-                        "v54.0",
-                        0.1
-                    ],
-                    [
-                        "v52.0",
-                        0.95
-                    ],
-                    [
-                        "v51.0",
-                        0.15
-                    ],
-                    [
-                        "v50.0",
-                        0.1
-                    ],
-                    [
-                        "v48.0",
-                        0.31
-                    ],
-                    [
-                        "v47.0",
-                        0.12
-                    ]
-                ]
-            },
-            {
-                name: "Internet Explorer",
-                id: "Internet Explorer",
-                data: [
-                    [
-                        "v11.0",
-                        6.2
-                    ],
-                    [
-                        "v10.0",
-                        0.29
-                    ],
-                    [
-                        "v9.0",
-                        0.27
-                    ],
-                    [
-                        "v8.0",
-                        0.47
-                    ]
-                ]
-            },
-            {
-                name: "Safari",
-                id: "Safari",
-                data: [
-                    [
-                        "v11.0",
-                        3.39
-                    ],
-                    [
-                        "v10.1",
-                        0.96
-                    ],
-                    [
-                        "v10.0",
-                        0.36
-                    ],
-                    [
-                        "v9.1",
-                        0.54
-                    ],
-                    [
-                        "v9.0",
-                        0.13
-                    ],
-                    [
-                        "v5.1",
-                        0.2
-                    ]
-                ]
-            },
-            {
-                name: "Edge",
-                id: "Edge",
-                data: [
-                    [
-                        "v16",
-                        2.6
-                    ],
-                    [
-                        "v15",
-                        0.92
-                    ],
-                    [
-                        "v14",
-                        0.4
-                    ],
-                    [
-                        "v13",
-                        0.1
-                    ]
-                ]
-            },
-            {
-                name: "Opera",
-                id: "Opera",
-                data: [
-                    [
-                        "v50.0",
-                        0.96
-                    ],
-                    [
-                        "v49.0",
-                        0.82
-                    ],
-                    [
-                        "v12.1",
-                        0.14
-                    ]
-                ]
-            }
+
         ]
     }
 }
@@ -295,19 +80,75 @@ export default {
       let energyPercentage = (footprint.energyTotal /footprint.combinedTotal) * 100
       let dietPercentage = (footprint.dietTotal / footprint.combinedTotal) * 100
 
+      //drilldown
+      let travelDrillCar = (footprint.car / footprint.travelTotal) * 100
+      let travelDrillTrain = (footprint.train / footprint.travelTotal) * 100
+      let travelDrillPlane = (footprint.plane / footprint.travelTotal) * 100
+
+      let energyDrillElectriciy = (footprint.electricity / footprint.energyTotal) * 100
+      let energyDrillGas = (footprint.gas / footprint.energyTotal) * 100
+      let energyDrillHybrid = (footprint.hybrid / footprint.energyTotal) * 100
+
+      let dietDrillMeat = (footprint.meat / footprint.dietTotal) * 100
+      let dietDrillVeg = (footprint.veg / footprint.dietTotal) * 100
+
+
 
       this.chartOptions.series[0].data.push({
         name: "Travel",
-        y: travelPercentage
+        y: travelPercentage,
+        drilldown: "Travel"
       })
       this.chartOptions.series[0].data.push({
         name: "Energy",
-        y: energyPercentage
+        y: energyPercentage,
+        drilldown: "Energy"
       })
       this.chartOptions.series[0].data.push({
         name: "Diet",
-        y: dietPercentage
+        y: dietPercentage,
+        drilldown: "Diet"
       })
+      debugger
+      this.chartOptions.drilldown.series.push(
+        {
+            name: "Travel",
+            id: "Travel",
+            data: [
+              ["Car",
+              travelDrillCar
+              ],
+              ["Train",
+            travelDrillTrain],
+              ["Plane",
+            travelDrillPlane]
+            ]
+        },
+        {
+          name: "Energy",
+          id: "Energy",
+          data: [
+            ["Electricity",
+            energyDrillElectriciy
+            ],
+            ["Gas",
+          energyDrillGas],
+            ["Hybrid",
+          energyDrillHybrid]
+          ]
+        },
+        {
+          name: "Diet",
+          id: "Diet",
+          data: [
+            ["Meat",
+            dietDrillMeat
+            ],
+            ["Veg",
+          dietDrillVeg]
+          ]
+        }
+      )
 
     }
   },
@@ -318,7 +159,6 @@ export default {
     eventBus.$on('footprint-added', (footprint)=> {
       this.footprints.push(footprint)
       this.chartOptions.series[0].data.length = 0
-      debugger
       this.chartOptions.title = {text: `CO2 Footprint for ${footprint.date}`}
       this.percentage(footprint)
 
