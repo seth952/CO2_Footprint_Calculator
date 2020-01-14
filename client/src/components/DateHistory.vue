64cb8a025340c15bf5b018e4b72f050b9a8b4fbb
User <user@Users-MacBook-Pro.local>
2020-01-14 13:17:11 +0000

    breakdown component added

--- client/src/components/DateHistory.vue
+++ client/src/components/DateHistory.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="">
-    <select id="dateHistory">
+    <select @change="handleChange" v-model="selectedDate">
       <option v-for="(foot, index) of footprints" :key="index" :value="foot">
-        Previous entry: {{foot.name}}
+        Previous Entry: {{foot.date}}
         </option>
 
       </select>
 
 
+
+      <breakdown :breakdown="selectedDate"/>
     </div>
   </template>
   <script>
+  import Breakdown from './Breakdown';
   import {eventBus} from '../main.js';
   import HotelService from '@/services/UserDataService';
   export default {
-    props: ['footprints']
+    props: ['footprints'],
+    data(){
+      return{
+        selectedDate: null
+      }
+    },
+      components:{
+        'breakdown': Breakdown
+      },
 
+    methods: {
+      handleChange(e){
+      eventBus.$emit('selected-date', this.selectedDate)
+    }
   }
+}
+
   </script>
   <style lang="scss" scoped>
   </style>
